d1e666f4e7aad439099279790f842e99c2827fa4
Alejandro Zubiri <79642058+azubirif@users.noreply.github.com>
2025-03-24 12:38:39 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,4 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<a href="/blog">Aquí pal blog</a>
+<a href="/template-svkit-gh/blog">Aquí pal blog</a>
